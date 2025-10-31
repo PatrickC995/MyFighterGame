ee037eb688171fa4ef03800e0695846ea1fdd669
PatrickC995 <campbellpatrick2004@gmail.com>
2025-10-31 13:41:05 +0000

    My First Commit

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main(String[] args) {
+    System.out.println("Welcome to My Fighter Game");
+}
