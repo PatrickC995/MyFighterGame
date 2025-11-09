68fa90f8c5491383e6e5338fede5f966ccc3addb
PatrickC995 <campbellpatrick2004@gmail.com>
2025-11-09 18:12:35 +0000

    added unit tests for player class

--- src/UnitTests/PlayerTest.java
+++ src/UnitTests/PlayerTest.java
@@ -39,4 +39,37 @@ public class PlayerTest {
         p.setName(testName);
         assertEquals(testName, p.getName(), "getName should return the value set with setName");
     }
+
+    @Test
+    public void defaultHealthIs100_setGetHealth() {
+
+        // default should be a 100
+        assertEquals(100, p.getHealth(), "default health should be 100");
+
+        int testHealth = 200;
+        p.setHealth(testHealth);
+        assertEquals(testHealth, p.getHealth(), "health should be 200");
+    }
+
+    @Test
+    public void defaultDamageIs10_setGetDamage() {
+
+        // default damage should be a 100
+        assertEquals(10, p.getDamage(), "default damage should be 10");
+
+        // set damage to 200
+        int testDamage = 100;
+        p.setDamage(testDamage);
+        assertEquals(testDamage, p.getDamage(), "damage should be 100");
+    }
+
+    @Test
+    public void defaultMaxHealthIs100_maxHealthTheSameAsHealth() {
+
+        // default max health should be a 100
+        assertEquals(100, p.getMaxHealth(), "maxHealth should be 100");
+
+        // max health should be the same as health
+        assertEquals(p.getHealth(), p.getMaxHealth(), "maxHealth should be 100");
+    }
 }
