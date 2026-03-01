869a2b7468cfec50fcc7eaf57deabebb7d88abbe
PatrickC995 <campbellpatrick2004@gmail.com>
2026-03-01 16:13:51 +0000

    added damage potion

--- src/classes/Potion.java
+++ src/classes/Potion.java
@@ -5,11 +5,19 @@ public class Potion {
     private int healthPotion;
     private int healthPotionCount;
 
+    // Damage Potion
+    private int damagePotion;
+    private int damagePotionCount;
+
     //Getters and Setters
     public int getHealthPotion() {return healthPotion;}
     public int getHealthPotionCount() {return healthPotionCount;}
+    public int getDamagePotion() {return damagePotion ;}
+    public int getDamagePotionCount() {return damagePotionCount;}
 
     public void setHealthPotion(int healthPotion) {this.healthPotion = healthPotion;}
     public void setHealthPotionCount(int healthPotionCount) {this.healthPotion = healthPotionCount;}
+    public void setDamagePotion(int damagePotion) {this.damagePotion = damagePotion;}
+    public void setDamagePotionCount(int damagePotionCount) {this.damagePotionCount = damagePotionCount;}
 
 }
