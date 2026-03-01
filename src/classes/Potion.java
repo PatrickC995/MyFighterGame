9dccc349eaa678ad7834dc84f48dcfb9cf1cd1d7
PatrickC995 <campbellpatrick2004@gmail.com>
2026-03-01 16:10:49 +0000

    added health potion

---
+++ src/classes/Potion.java
@@ -0,0 +1,15 @@
+package classes;
+
+public class Potion {
+    // Health Potion
+    private int healthPotion;
+    private int healthPotionCount;
+
+    //Getters and Setters
+    public int getHealthPotion() {return healthPotion;}
+    public int getHealthPotionCount() {return healthPotionCount;}
+
+    public void setHealthPotion(int healthPotion) {this.healthPotion = healthPotion;}
+    public void setHealthPotionCount(int healthPotionCount) {this.healthPotion = healthPotionCount;}
+
+}
