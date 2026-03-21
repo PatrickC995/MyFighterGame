ff3ab15c4779b3a3bd1a5961f52a11e59cb6d62e
PatrickC995 <campbellpatrick2004@gmail.com>
2026-03-21 12:13:05 +0000

    clean up potion class

--- src/classes/Potion.java
+++ src/classes/Potion.java
@@ -1,49 +1,37 @@
 package classes;
 
-import MyLibrary.randomNumberGenerator;
-
 import java.util.Random;
 
 public class Potion {
 
     // Health Potion
     private int healthPotion;
-    private int healthPotionCount;
 
     // Damage Potion
     private int damagePotion;
-    private int damagePotionCount;
 
     //Getters and Setters
     public int getHealthPotion() {return healthPotion;}
-    public int getHealthPotionCount() {return healthPotionCount;}
     public int getDamagePotion() {return damagePotion ;}
-    public int getDamagePotionCount() {return damagePotionCount;}
 
     public void setHealthPotion(int healthPotion) {this.healthPotion = healthPotion;}
-    public void setHealthPotionCount(int healthPotionCount) {this.healthPotion = healthPotionCount;}
     public void setDamagePotion(int damagePotion) {this.damagePotion = damagePotion;}
-    public void setDamagePotionCount(int damagePotionCount) {this.damagePotionCount = damagePotionCount;}
 
     //Constructor
     public Potion() {
         this.healthPotion = 5;
-        this.healthPotionCount = 0;
         this.damagePotion = 2;
-        this.damagePotionCount = 0;
     }
 
-    public Potion(int healthPotion, int healthPotionCount, int damagePotion, int damagePotionCount) {
+    public Potion(int healthPotion, int damagePotion) {
       this.healthPotion = healthPotion;
-      this.healthPotionCount = healthPotionCount;
       this.damagePotion = damagePotion;
-      this.damagePotionCount = damagePotionCount;
     }
 
 
     public String randomPotion() {
         Random random = new Random();
-        int randomNumber = random.nextInt(2) + 1;
+        int randomNumber = random.nextInt(4) + 1;
 
         switch (randomNumber) {
             case 1:
@@ -53,11 +41,5 @@ public class Potion {
             default:
                 return "";
         }
-    }
-
-    public void potionDisplay() {
-        System.out.println(
-                "You have " + healthPotionCount + "health potions\n " +
-                "You have " + damagePotionCount + "damage potions");
     }
 }
